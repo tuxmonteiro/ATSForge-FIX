d035708b976f9e2cfd4e3b736e76094cc18481ee
Marcelo Monteiro <marcelotmonteiro@gmail.com>
2011-12-27 02:14:06 -0200

    atsforgefix.server sources

---
+++ atsforgefix.server/src/main/java/org/atsforge/fix/server/MainServer.java
@@ -0,0 +1,32 @@
+package org.atsforge.fix.server;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public class MainServer 
+{
+    public static void main( String[] args )
+    {
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool()));
+
+        // Set up the pipeline factory.
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() throws Exception {
+                return Channels.pipeline(new FixServerHandler());
+            }
+        });
+
+        // Bind and start to accept incoming connections.
+        bootstrap.bind(new InetSocketAddress(8080));
+    }
+}
